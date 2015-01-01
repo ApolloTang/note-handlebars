8c1cc69ecfba89e60521936f62b6952c0fcda10d
Apollo Tang <tang.apollo@gmail.com>
2015-01-01 02:01:09 -0500

    initial commit

---
+++ precompile/test.beautify.js
@@ -0,0 +1,22 @@
+(function() {
+    var template = Handlebars.template,
+        templates = Handlebars.templates = Handlebars.templates || {};
+    templates['test'] = template({
+        "compiler": [6, ">= 2.0.0-beta.1"],
+        "main": function(depth0, helpers, partials, data) {
+            var helper, functionType = "function",
+                helperMissing = helpers.helperMissing,
+                escapeExpression = this.escapeExpression;
+            return "<div class=\"entry\">\n    <h1>" + escapeExpression(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : helperMissing), (typeof helper === functionType ? helper.call(depth0, {
+                "name": "title",
+                "hash": {},
+                "data": data
+            }) : helper))) + "</h1>\n    <div class=\"body\">\n      " + escapeExpression(((helper = (helper = helpers.body || (depth0 != null ? depth0.body : depth0)) != null ? helper : helperMissing), (typeof helper === functionType ? helper.call(depth0, {
+                "name": "body",
+                "hash": {},
+                "data": data
+            }) : helper))) + "\n    </div>\n</div>\n";
+        },
+        "useData": true
+    });
+})();
